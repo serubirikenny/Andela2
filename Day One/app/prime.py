c40ed60c2300399bcd545b23af23b7a269ce0f11
serubirikenny <kserubiri@gmail.com>
2017-07-03 17:53:17 +0300

    functional program to return prime numbers

---
+++ Day One/app/prime.py
@@ -0,0 +1,25 @@
+class Prime(object):
+
+    def check(self, n):
+        prime_list = []
+
+        for i in range(2, n):
+            j = 2
+            counter = 0
+
+            while j < i:
+                if i % j == 0:
+                    counter = 1
+                    i += 1
+
+                else:
+                    j += 1
+
+            if counter == 0:
+                prime.append(i)
+            else:
+                counter = 0
+        return prime_list
+        
+number = Prime()
+print(number.check(6))
